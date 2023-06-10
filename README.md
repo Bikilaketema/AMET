b452b841ac4d16e33e8f6f0679456094fccdb3a1
bikilaketema <bikilaketema94@gmail.com>
2023-06-10 20:03:51 +0300

    back after cleaning up

---
+++ README.md
@@ -0,0 +1,46 @@
+
+# AMET.com - Online Marketplace
+
+This repository contains the source code and documentation for AMET.com, an online marketplace that connects buyers and sellers of various products and services. The platform provides a user-friendly interface for users to buy and sell a wide range of items, including electronics, fashion, home goods, and more.
+
+https://drive.google.com/file/d/12iy84T6lcRkSJqg2BpBg5FsrK9UQ2Wmx/view?usp=sharing
+## Authors
+
+- [@bikilaketema](https://www.github.com/bikilaketema)
+
+
+## Introduction
+
+AMET.com aims to solve the problem of providing a user-friendly and efficient online marketplace for individuals and businesses to buy and sell various products and services. The platform includes features such as user authentication, product listings, shopping cart functionality, payment integration, a review and rating system, and messaging capabilities.
+
+The project is built using HTML, CSS, and JavaScript for the front-end, and a web framework Flask for the back-end. The database management system used is MySQL. The application follows a RESTful API architecture to ensure scalability, performance, and ease of maintenance.
+
+The products listed on the website are retrived from fakestore's API.
+
+## Features
+
+- User authentication: Users can create accounts, log in, and manage their profiles.
+- Product listings: Sellers can create listings with detailed descriptions and images for their products.
+- Shopping cart: Buyers can add items to their cart and proceed to checkout.
+- Payment integration
+- Review and rating system: Users can leave feedback and ratings for products and sellers.
+- Messaging system: Buyers and sellers can communicate with each other through the platform.
+- Search and filtering: Users can search for specific products and apply filters to narrow down their options.
+- Responsive design: The platform is optimized for various devices, including desktop and mobile.
+
+## Technologies used
+- Programming Languages: HTML, CSS, JavaScript, Python
+- Front-end Framework: React
+- Back-end Framework: Flask
+- Database Management: MySQL
+- Payment Gateway Integration: Chapa
+- Deployment: NGINX
+
+## Usage
+Once they open the website, users can perform the following actions:
+
+- Create an account or log in with existing credentials.
+- Browse product listings and search for specific items.
+- Add products to the shopping cart and proceed to checkout.
+- Pay securely using the integrated payment gateways (Telebirr, Chapa).
+- Leave
---
+++ app/app.py
@@ -0,0 +1,188 @@
+import bcrypt
+from flask import Flask, render_template, request, redirect, session
+import requests
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import auth
+import json
+import os
+
+app = Flask(__name__, static_folder='static')
+app.secret_key = 'your-secret-key'  # Add a secret key for session management
+
+# Initialize Firebase Admin SDK
+cred = credentials.Certificate('/home/bicky/AMET/app/serviceAccountKey.json')
+firebase_admin.initialize_app(cred)
+
+# Route to display the index.html template
+@app.route('/')
+def landing():
+    return render_template('landing.html')
+
+
+@app.route('/index')
+def index():
+    # Fetch product data from the FakeStore API
+    response = requests.get('https://fakestoreapi.com/products')
+    products = response.json()
+
+    return render_template('index.html', products=products)
+
+@app.route('/products')
+def products():
+    # Fetch product categories from the FakeStore API
+    response = requests.get('https://fakestoreapi.com/products/categories')
+    categories = response.json()
+
+    return render_template('products.html', categories=categories)
+
+@app.route('/category/<string:category>')
+def category(category):
+    url = f'https://fakestoreapi.com/products/category/{category}'
+    response = requests.get(url)
+    products = response.json()
+    return render_template('category.html', category=category, products=products)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+""" # Function to fetch product details from the Fake Store API
+def get_product_details(product_id):
+    url = f'https://fakestoreapi.com/products/{product_id}'
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        product = response.json()
+        return product
+    else:
+        return None
+
+# Route for product detail page
+@app.route('/product/<int:product_id>')
+def product_detail(product_id):
+    # Retrieve product details from the Fake Store API
+    product = get_product_details(product_id)
+
+    if product is None:
+        return render_template('404.html'), 404
+
+    return render_template('product_detail.html', product=product) """
+
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+
+        if len(password) < 6:
+            # Password is too short
+            return render_template('signup.html', error='Password must be at least 6 characters long.', email=email)
+
+        if password == confirm_password:
+            try:
+                user = auth.create_user(email=email, password=password)
+                session['user'] = user.uid  # Store the user ID in the session
+
+                # Create a dictionary with user information
+                user_data = {
+                    'email': email,
+                    'password': bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+                }
+
+                file_path = 'users.json'
+
+                # Check if the JSON file exists
+                if not os.path.exists(file_path):
+                    # Create the file if it doesn't exist
+                    with open(file_path, 'w') as file:
+                        json.dump([], file)
+
+                # Read existing user data from the JSON file
+                with open(file_path, 'r') as file:
+                    data = json.load(file)
+
+                # Add the new user data to the existing data
+                data.append(user_data)
+
+                # Write the updated JSON data
+                with open(file_path, 'w') as file:
+                    json.dump(data, file)
+
+                return redirect('/dashboard')
+            except auth.EmailAlreadyExistsError:
+                # User with the given email already exists
+                return render_template('signup.html', error='Email already exists.', email=email)
+            except Exception as e:
+                # Print error details for debugging
+                print(f"Error during signup: {str(e)}")
+                return render_template('signup.html', error='An error occurred during signup.', email=email)
+        else:
+            # Passwords do not match
+            return render_template('signup.html', error='Passwords do not match.', email=email)
+
+    return render_template('signup.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        try:
+            user = auth.get_user_by_email(email)
+            uid = user.uid
+
+            # Get the stored password from the JSON file
+            with open('users.json', 'r') as file:
+                user_data = json.load(file)
+                stored_password = next((data['password'] for data in user_data if data['email'] == email), None)
+
+            if stored_password is not None and bcrypt.checkpw(password.encode('utf-8'), stored_password.encode('utf-8')):
+                session['user'] = uid  # Store the user ID in the session
+                return redirect('/dashboard')
+            else:
+                # Incorrect password
+                return render_template('login.html', error='Incorrect password.', email=email)
+
+        except auth.UserNotFoundError:
+            # User with the given email doesn't exist
+            return render_template('login.html', error='Account associated with this email not found. Click the signup button and create one.', email=email)
+        except Exception as e:
+            # Print error details for debugging
+            print(f"Error during login: {str(e)}")
+            return render_template('login.html', error='An error occurred during login.', email=email)
+
+    return render_template('login.html')
+
+
+
+
+@app.route('/dashboard')
+def dashboard():
+    user_id = session.get('user')
+    if not user_id:
+        return redirect('/login')
+
+    # Get user data from Firebase
+    user = auth.get_user(user_id)
+    email = user.email
+    return render_template('dashboard.html', email=email)
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/login')
+
+if __name__ == '__main__':
+    app.run(host='10.9.212.157', port=5000)
